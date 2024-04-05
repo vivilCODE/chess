e64dd203687ab60fd43b655f76031247b7aa6320
Ville Desktop <ville_kalamies@hotmail.com>
2024-04-05 16:52:07 +0200

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # chess
+
+
+protoc --go_out=protobuf --go_opt=paths=source_relative --go-grpc_out=protobuf --go-grpc_opt=paths=source_relative chess.proto
---
+++ chessapi/server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/vivilCODE/chess/chessapi/protobuf"
+	"google.golang.org/grpc"
+)
+
+type myChessApi struct {
+	pb.UnimplementedChessApiServer
+}
+
+func (c *myChessApi) MakeMove(context.Context, *pb.MakeMoveRequest) (*pb.MakeMoveResponse, error) {
+	return &pb.MakeMoveResponse{Body: "response from server"}, nil
+}
+
+func main() {
+	// Set up listener to a port
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err)
+	}
+
+	// Initialise grpc server
+	grpcServer := grpc.NewServer()
+
+	// Register chess api
+	api := &myChessApi{}
+	pb.RegisterChessApiServer(grpcServer, api)
+
+	// Serve grpc
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve grpc: %v", err)
+	}
+}
